d5c66e9cee167b55db61b5b55ac552e9b442d974
huyenlinh164 <huyenlinhw164@gmail.com>
2025-11-04 14:42:33 +0700

    bai thuc hanh 4

---
+++ DemoMVC396/Controllers/DemoController.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC396.Controllers
+{
+    public class DemoController : Controller
+    {
+        //  Trả về ViewResult
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Xin chào từ ViewBag!";
+            ViewData["Note"] = "Thông báo từ ViewData!";
+            TempData["Alert"] = "Dữ liệu từ TempData (vẫn tồn tại sau Redirect).";
+            return View();
+        }
+        
+        public IActionResult ShowTempData()
+        {
+            // TempData giữ được dữ liệu từ action Index
+            return View();
+        }
+        //  Trả về RedirectResult
+        public IActionResult GoToGoogle()
+        {
+            return Redirect("https://www.google.com");
+        }
+
+        //  Trả về RedirectToActionResult
+        public IActionResult GoHome()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        //  Trả về JsonResult
+        public IActionResult GetJson()
+        {
+            var data = new 
+            { 
+                Id = 1,
+                Name = "Linh",
+                Message = "Xin chào",
+                
+             };
+            return Json(data);
+        }
+        //FileResult — Trả về một file
+           public IActionResult FileExample()
+        {
+            
+                return StatusCode(404, "Không tìm thấy file example.pdf trong wwwroot.");
+        }
+
+        //  Trả về StatusCodeResult
+        public IActionResult ShowError()
+        {
+            return StatusCode(404, "Trang không tồn tại!");
+        }
+
+    }
+}
--- DemoMVC396/Controllers/HomeController.cs
+++ DemoMVC396/Controllers/HomeController.cs
@@ -2,30 +2,29 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MvcMovie.Models;
 
-namespace MvcMovie.Controllers;
-
-public class HomeController : Controller
+namespace MvcMovie.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+        [HttpPost]
+        public IActionResult Index(string FullName, string Address)
+        {
+            string strOutput = "Xin chào " + FullName + " đến từ " + Address;
+            ViewBag.Message = strOutput;
+            return View();
+        }
+    } 
 }
+
